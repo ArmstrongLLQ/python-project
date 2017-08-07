0d925577e9fca36ac542d345a79aa345fc7cb6fe
ArmstrongLLQ <armstrong_lab@163.com>
2017-08-07 17:13:24 +0800

    multi_thread

--- dianziyisuo/test.py
+++ dianziyisuo/test.py
@@ -8,6 +8,8 @@ import json
 import requests
 import urllib
 import pymysql
+import threading
+import queue
 
 class DOAJ:
     def __init__(self, base_url, auth, new_filter):
@@ -41,21 +43,25 @@ class DOAJ:
         return db, cursor
 
     # 将数据库插入语句封装成函数
-    def __insertMysql(self, each, db, cursor):
+    def __insertMysql(self, each, db, cursor, insert_success):
         # sql语句
-        sql = """INSERT INTO doaj_data(`id`, `title`, `title_translation`, `abstract`, `abstract_translation`, `year`, `url`, `start_page`, `end_page`, `article_created_date`, `article_last_updated`, `journals_publisher`, `journals_language`, `journals_licenseId`, `journals_title`, `journals_country`, `journals_number`, `journals_volume`, `journals_issns`, `journals_create_date`, `term`, `term_code`, `term_l1`, `keyword`, `keyword_translation`, `author_name`, `author_affiliation`, `author_email`, `identifier_type`, `identifier_identifierId`, `license_type`, `license_title`, `license_url`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        sql = """INSERT INTO doaj_data2(`id`, `title`, `title_translation`, `abstract`, `abstract_translation`, `year`, `url`, `start_page`, `end_page`, `article_created_date`, `article_last_updated`, `journals_publisher`, `journals_language`, `journals_licenseId`, `journals_title`, `journals_country`, `journals_number`, `journals_volume`, `journals_issns`, `journals_create_date`, `term`, `term_code`, `term_l1`, `keyword`, `keyword_translation`, `author_name`, `author_affiliation`, `author_email`, `identifier_type`, `identifier_identifierId`, `license_type`, `license_title`, `license_url`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         my_value = (each['id'], each['title'], each['title_translation'], each['abstract'], each['abstract_translation'], each['year'], each['url'], each['start_page'], each['end_page'], each['article_created_date'], each['article_last_updated'], each['journals_publisher'], each['journals_language'], each['journals_licenseId'], each['journals_title'], each['journals_country'], each['journals_number'], each['journals_volume'], each['journals_issns'], each['journals_create_date'], each['term'], each['term_code'], each['term_l1'], each['keyword'], each['keyword_translation'], each['author_name'], each['author_affiliation'], each['author_email'], each['identifier_type'], each['identifier_identifierId'], each['license_type'], each['license_title'], each['license_url'])
         try:
             # 执行sql语句
             cursor.execute(sql, my_value)
             # 提交到数据库执行
             db.commit()
+            insert_success += 0
             print('insert success')
         except Exception:
             # 如果发生错误则回滚
             print('insert fail')
-            raise
+            # raise
+            insert_success += 1
             db.rollback()
+
+        return insert_success
 
     # 进行数据爬取，并存入数据库
     def insertDoajDatabase(self, my_host="172.16.155.11", my_username="doaj", my_keyword="Doa123!@#j", my_database="doaj"):
@@ -70,25 +76,54 @@ class DOAJ:
         #pre_page_url = str(rsp.json()['previous'])
         #print(next_page_url)
         #print(pre_page_url)
+        insert_success = 0
         # 将当前页面的数据存入数据库
         if rsp.ok:
             for each in rsp.json()['results']:
                 #print(each['id'])
-                self.__insertMysql(each, db, cursor)
+                self.__insertMysql(each, db, cursor, insert_success)
 
         while next_page_url != 'None':
-            rsp = requests.get(next_page_url, auth=self.__auth,)
+            rsp = requests.get(next_page_url, auth=self.__auth)
             rsp.encoding = 'utf-8'
             next_page_url = str(rsp.json()['next'])
             #print(next_page_url)
             #print(rsp)
             for each in rsp.json()['results']:
-                #print(each['id'])
-                self.__insertMysql(each, db, cursor)
+                self.__insertMysql(each, db, cursor, insert_success)
 
         # 关闭数据库连接
         cursor.close()
-        db.close() 
+        db.close()
+
+    def insertNewData(self, my_host="172.16.155.11", my_username="doaj", my_keyword="Doa123!@#j", my_database="doaj"):
+        db, cursor = self.__connectDatabase(my_host, my_username, my_keyword, my_database)
+        insert_success = 0
+        # 获取网页数据
+        rsp = requests.get(urllib.parse.urljoin(self.__base_url, '/doaj/'), auth=self.__auth, params=self.__filter)
+        rsp.encoding = 'utf-8'
+        data_count = int(str(rsp.json()['count']))
+        last_page = data_count // 20 + 1
+        last_page_url = "http://121.42.164.187:8088/doaj/?page=%s&term_l1=%s&year=%s" % (last_page, self.__filter['term_l1'], self.__filter['year'])
+        rsp = requests.get(last_page_url, auth=self.__auth)
+        rsp.encoding = 'utf-8'
+        
+        if rsp.ok:
+            for each in rsp.json()['results']:
+                insert_success = self.__insertMysql(each, db, cursor, insert_success)
+
+        while insert_success == 0:
+            last_page -= 1
+            last_page_url = "http://121.42.164.187:8088/doaj/?page=%s&term_l1=%s&year=%s" % (last_page, self.__filter['term_l1'], self.__filter['year'])
+            rsp = requests.get(last_page_url, auth=self.__auth)
+            rsp.encoding = 'utf-8'
+            for each in rsp.json()['results']:
+                insert_success = self.__insertMysql(each, db, cursor, insert_success)
+
+        # 关闭数据库连接
+        cursor.close()
+        db.close()
+
 
     # 数据库查询操作，传入查询语句和数据库信息（默认）
     def selectFromDatabase(self, select_sql, my_host="172.16.155.11", my_username="doaj", my_keyword="Doa123!@#j", my_database="doaj"):
@@ -150,36 +185,61 @@ class DOAJ:
 
         # 关闭数据库连接
         cursor.close()
-        db.close()    
+        db.close()      
 
-    def getCount(self):
-         # 获取网页数据
-        rsp = requests.get(urllib.parse.urljoin(self.__base_url, '/doaj/'), auth=self.__auth, params=self.__filter)
-        rsp.encoding = 'utf-8'
+class CrawlThread(threading.Thread):
+    def __init__(self, filter_queue):
+        threading.Thread.__init__(self)
+        self.filter_queue = filter_queue
 
-        # 得到下一页要爬取数据的url
-        term_count = str(rsp.json()['count'])
+    def run(self):
+        print('thread start----------------------------------')
+        filter1 = self.filter_queue.get()
+        test1 = DOAJ('http://121.42.164.187:8088/', ('xxjs', '123456a?'), filter1)
+        self.filter_queue.task_done()
+        test1.insertNewData("172.16.155.11", "doaj", "Doa123!@#j", "doaj")
+        print('thread end-------------------------------------')
 
-        return term_count
-
-
+            
 if __name__ == '__main__':
+    filter_queue = queue.Queue()
     # 在此可以修改用户名、密码以及fileter参数
     base_url = 'http://121.42.164.187:8088/'
     auth = ('xxjs', '123456a?')
     term_l1 = ['Science', 'Technology', 'Military Science']
     year = ['2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000']
     
-    count_sum = 0
+    
     for term in term_l1:
         for ayear in year:
             new_filter = {'term_l1':term, 'year':ayear}
-            test1 = DOAJ(base_url, auth, new_filter)
-            tmp_count = int(test1.getCount())
-            count_sum += tmp_count
-            print(count_sum)
+            filter_queue.put(new_filter)
 
-    print(count_sum)
+    thread1 = CrawlThread(filter_queue)
+    thread2 = CrawlThread(filter_queue)
+    thread3 = CrawlThread(filter_queue)
+    thread4 = CrawlThread(filter_queue)
+    thread5 = CrawlThread(filter_queue)
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
+
+    filter_queue.join()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
+    thread5.join()
 
 
- 
+
+
+    # 创建一个对象，可以通过setBaseUrl/setAuth/setFilter方法对参数进行修改
+    
+    
+    #test1.selectFromDatabase('select * from doaj_data')
+    #test1.updateDatabase('update doaj_data set title = "test title" where id = 54')
+    #test1.deleteDatabase('delete from doaj_data where id = 21')
